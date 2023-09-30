1cb1d428c1c05936c48b6148eb4913f1be11bb78
ankitkumardev25 <ankitkumar1999825@gmail.com>
2023-09-30 21:46:29 +0530

    games-db

---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+//PascalCasing - first letter of every word to be capital
+const Message = () => {
+  //JSX - Javascript XML
+  const name = 'Ankit'
+  return <h1>Hello {name}</h1>
+}
+export default Message
